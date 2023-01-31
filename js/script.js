e599f9ce4520690fcff8f6685c094287160b98e5
Robert Fydrych <fydrych.robert.w@gmail.com>
2023-01-31 20:08:30 +0000

    js changed

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-let formElement = document.querySelector(".js-form");
-let currencyFrom = document.querySelector(".js-currencyFrom");
-let currencyTo = document.querySelector(".js-currencyTo");
-let amount = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
+{
+    const formElement = document.querySelector(".js-form");
+    const currencyFrom = document.querySelector(".js-currencyFrom");
+    const currencyTo = document.querySelector(".js-currencyTo");
+    const amount = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
 
-let eur = 4.72;
-let gbp = 5.40;
-let pln = 1;
+    const eur = 4.72;
+    const gbp = 5.40;
+    const pln = 1;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    switch (currencyFrom.value) {
-        case "gbp":
-            result = amount.value * gbp;
-            break;
-        case "pln":
-            result = amount.value * pln;
-            break;
-        case "eur":
-            result = amount.value * eur;
-            break;
-    }
-    switch (currencyTo.value) {
-        case "gbp":
-            result /= gbp;
-            break;
-        case "pln":
-            result /= pln;
-            break;
-        case "eur":
-            result /= eur;
-            break;
-    }
-    resultElement.innerText = result.toFixed(2);
-});
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        switch (currencyFrom.value) {
+            case "gbp":
+                result = amount.value * gbp;
+                break;
+            case "pln":
+                result = amount.value * pln;
+                break;
+            case "eur":
+                result = amount.value * eur;
+                break;
+        }
+        switch (currencyTo.value) {
+            case "gbp":
+                result /= gbp;
+                break;
+            case "pln":
+                result /= pln;
+                break;
+            case "eur":
+                result /= eur;
+                break;
+        }
+        resultElement.innerText = result.toFixed(2);
+    });
+}
